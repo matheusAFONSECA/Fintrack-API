e205bfc041c43d4f2a40a4a66ea4b8e71ae5596f
Álvaro Lúcio Almeida Ribeiro <alvaroluciorib@gmail.com>
2024-10-17 14:05:44 -0300

    feat(api): implementing the register function
    The register function is now functional.

--- src/fintrack_api/services/user/create.py
+++ src/fintrack_api/services/user/create.py
@@ -6,27 +6,26 @@ from fintrack_api.models.userModels import UserIn
 
 
 async def create_user(user: UserIn) -> None:
-    query = """INSERT INTO "user"(cpf, name, email, password, phone, birth) VALUES (%(cpf)s, %(name)s, %(email)s, %(password)s, %(phone)s, %(birth)s);"""
+    query = """INSERT INTO "user"(name, email, password) 
+               VALUES (%(name)s, %(email)s, %(password)s);"""
     parameters = {
-        'cpf': user.cpf,
         'name': user.name,
         'email': user.email,
         'password': user.password,
-        'phone': user.phone,
-        'birth': user.birth,
     }
     try:
         with connect() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(query, parameters)
             conn.commit()
-
     except IntegrityError as e:
         error = re.search(r'Key \((.*?)\)=\((.*?)\) already exists', e.pgerror)
-        raise HTTPException(status_code=400, detail=f"User with {error.group(1)} {error.group(2)} already exists.")
+        raise HTTPException(
+            status_code=400, 
+            detail=f"User with {error.group(1)} {error.group(2)} already exists."
+        )
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
     finally:
         cursor.close()
         conn.close()
