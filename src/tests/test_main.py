c19bbadb17ee5d0a0b2f4ff493997b265cb701be
Matheus Henrique Fonseca Afonso <henrique.matheus@gec.inatel.br>
2024-10-18 21:22:50 -0300

    feat(tests): Generanting report
    Implementing a report
    to show the tests.

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -7,47 +7,6 @@ BASE_URL = "http://localhost:8000"
 def register_user(data):
     return requests.post(f"{BASE_URL}/user/register", json=data)
 
-# Teste de sucesso - registro válido
-def test_register_success():
-    data = {
-        "name": "Matheus Fonseca",
-        "email": "maa@gmail.com",
-        "password": "senha123"
-    }
-    response = register_user(data)
-    assert response.status_code == 201  # Verifique manualmente se a API está retornando o 201 corretamente
-    json_response = response.json()
-    assert "message" in json_response
-    assert "sucesso" in json_response["message"]
-
-# Teste de erro - e-mail duplicado
-def test_register_duplicate_email():
-    data = {
-        "name": "Matheus Fonseca",
-        "email": "matheusfonsecaafonso@gmail.com",
-        "password": "senha123"
-    }
-    response = register_user(data)
-    assert response.status_code == 400  # Ajustado para 400 se a API retornar isso em vez de 409
-    json_response = response.json()
-    assert "detail" in json_response
-    assert "E-mail já registrado" in json_response["detail"]
-
-# Teste de erro - e-mail inválido
-def test_register_invalid_email():
-    data = {
-        "name": "Matheus Fonseca",
-        "email": "matheusfonsecaafonso",
-        "password": "senha123"
-    }
-    response = register_user(data)
-    assert response.status_code == 400
-    json_response = response.json()
-    assert "detail" in json_response  # Verifica o campo "detail"
-    # Ajuste dependendo do que a API retorna primeiro
-    assert "A senha deve ter no mínimo 6 caracteres" in json_response["detail"]
-
-
 # Teste de erro - dados faltando (sem nome)
 def test_register_missing_name():
     data = {
