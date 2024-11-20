f4400e7805a2fa4e7b05e4e97af8d11c1a8a09d5
Álvaro Lúcio Almeida Ribeiro <alvaroluciorib@gmail.com>
2024-11-20 09:58:23 -0300

    update(main): Update sys.path configuration Updated sys.path to ensure correct module imports and resolve path-related issues.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import uvicorn
 from fastapi import FastAPI
